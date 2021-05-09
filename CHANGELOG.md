a80bcafab3a52fab09151dc06fbf0736a169e472
Michel Davit <michel.davit@gmail.com>
2021-05-09 18:58:33 +0200

    Cats effect 3 (#25)
    * Upgrade to CE3
    
    * Remove unused dependency
    
    * Remove dead code
    
    * Random interface for testing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 ## Unreleased
 
+- Update to cats-effect 3
+- scan and register on all interface by default
+
 ## v0.1.0 (2021-01-13)
 
-Initial release
+- Initial release
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,18 +3,16 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val Decline               = "1.3.0"
-    val MUnit                 = "0.7.23"
-    val ScalaCollectionCompat = "2.4.3"
-    val Taxonomy              = "0.3.0"
+    val MUnitCE3              = "1.0.0"
+    val ScalaCollectionCompat = "2.4.2"
+    val Taxonomy              = "1.0.0"
   }
 
-  val Decline               = "com.monovore"           %% "decline-effect"          % Versions.Decline
   val ScalaCollectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % Versions.ScalaCollectionCompat
   val Taxonomy              = "fr.davit"               %% "taxonomy-fs2"            % Versions.Taxonomy
 
   object Test {
-    val MUnit = "org.scalameta" %% "munit" % Versions.MUnit % "it,test"
+    val MUnitCE3 = "org.typelevel" %% "munit-cats-effect-3" % Versions.MUnitCE3 % "it,test"
   }
 
 }
