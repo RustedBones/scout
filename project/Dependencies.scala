66c1c58b1a4e660630cc4793ab25541b20391171
Scala Steward <scala_steward@virtuslab.com>
2023-07-12 22:52:36 +0000

    Reformat with scalafmt 3.7.8
    Executed command: scalafmt --non-interactive

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,16 +1,12 @@
-import sbt._
+import sbt.*
 
-object Dependencies {
+object Dependencies:
 
-  object Versions {
+  object Versions:
     val MUnitCE3 = "1.0.7"
     val Taxonomy = "1.2.1"
-  }
 
   val Taxonomy = "fr.davit" %% "taxonomy-fs2" % Versions.Taxonomy
 
-  object Test {
+  object Test:
     val MUnitCE3 = "org.typelevel" %% "munit-cats-effect-3" % Versions.MUnitCE3 % "it,test"
-  }
-
-}
